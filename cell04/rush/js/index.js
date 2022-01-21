558b78d379e1c2ce8a47d1359c2ff2852933d9a9
Miguel Ángel Adorna Ruiz (madorna-) <madorna-@student.42madrid.com>
2022-01-21 15:15:13 +0100

    Removed console.log

--- cell04/rush/js/index.js
+++ cell04/rush/js/index.js
@@ -3,8 +3,8 @@
 window.addEventListener('load', () => {
 	document.querySelectorAll('nav ul li a').forEach((element) => {
 		element.addEventListener('click', (e) => {
-			window.scrollTo(0, document.getElementById(e.target.hash.split('#')[1]).offsetTop - 75)
 			e.preventDefault()
+			window.scrollTo(0, document.getElementById(e.target.hash.split('#')[1]).offsetTop - 75)
 			return false
 		})
 	})
